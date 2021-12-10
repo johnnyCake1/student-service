3409745a6253e2dddf0c7e8e8de6950714bb72ba
Zheenbek Akimzhanov <zheenbek.akimzhanov@gmail.com>
2021-12-10 19:18:55 +0100

    Finalize deployment to VPS

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 echo "Deploying changes..."
 
 # pull from git
-git pull
+git pull &&
 
 # build
-./mvnw clean package -DskipTests
-cp target/student-service-0.0.1-SNAPSHOT.jar src/main/docker
+./mvnw clean package -DskipTests &&
+cp target/student-service-0.0.1-SNAPSHOT.jar src/main/docker &&
 
 # shutdown existing containers
-cd src/main/docker
-docker-compose down
-docker rmi docker-spring-boot-postgres:latest
+cd src/main/docker &&
+docker-compose down &&
+docker rmi docker-spring-boot-postgres:latest &&
 
 # start new containers
 docker-compose up -d
